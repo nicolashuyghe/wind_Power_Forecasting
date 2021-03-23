e2f4d42445b1ad3af7753570a6ac099abf4d2899
Nicolas Huyghe <nicolas.huyghe@gadz.org>
2021-03-23 18:37:10 +0100

    Streamlit app initialisation and basic sktime model to forecast wind power

---
+++ wind_Power_Forecasting/entsoe_client.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import streamlit as st
+from dotenv import load_dotenv
+from entsoe import EntsoePandasClient
+from entsoe.mappings import TIMEZONE_MAPPINGS
+
+#Using the caching mechanism of Streamlit,
+#to download energy data from the ENTSOE API only when necessary
+@st.cache
+def get_energy_data(country_code):
+    load_dotenv()
+    token = os.environ['ENTSOE_TOKEN']
+    client = EntsoePandasClient(api_key=token)
+    end = pd.Timestamp.now(tz=TIMEZONE_MAPPINGS[country_code])
+    start = end - pd.DateOffset(months=1)
+    df = client.query_generation(country_code, start=start,end=end, psr_type=None)
+    #Resampling the dataframe with an hourly frequency
+    df = df.resample('H').mean()
